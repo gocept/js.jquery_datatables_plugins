72194ecfc4cd5ac9e63161b358013194a7426536
Steffen Allner <sa@gocept.com>
2020-01-10 09:36:26 +0100

    This breaks locally but might help on travis.

--- js/jquery_datatables_plugins/__init__.py
+++ js/jquery_datatables_plugins/__init__.py
@@ -1,9 +1,9 @@
 from fanstatic import Library, Resource
-from js.jquery_datatables import datatables_js
+from js.jquery_datatables import jquery_datatables_js
 
 library = Library('jquery_datatables_plugins', 'resources')
 
 column_filter = Resource(
     library, 'columnfilter/media/js/jquery.dataTables.columnFilter.js',
-    depends=[datatables_js],
+    depends=[jquery_datatables_js],
     minified='columnfilter/media/js/jquery.dataTables.columnFilter.min.js')
